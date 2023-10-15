eb91565f7bd27ee7e3344a97065b0ee44755a1b4
mamomental <healhunter@gmail.com>
2023-10-15 10:26:00 +0900

    Update main.js

--- 001/js/main.js
+++ 001/js/main.js
@@ -1,17 +1,9 @@
+import dino from "./dino.js"
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
-var dino = {
-	x : 10,
-	y : 200,
-	width : 50,
-	height : 50,
-	draw() {
-		ctx.fillStyle = 'green';
-		ctx.fillRect(this.x, this.y, this.width, this.height);
-	}
-}
 dino.draw();
