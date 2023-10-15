193b2812ee3e9c2a7de6145292b3db2a8b4f0456
mamomental <healhunter@gmail.com>
2023-10-15 10:12:01 +0900

    Update main.js

--- 001/js/main.js
+++ 001/js/main.js
@@ -1,1 +1,17 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+var dino = {
+	x : 10,
+	y : 200,
+	width : 50,
+	height : 50,
+	draw() {
+		ctx.fillStyle = 'green';
+		ctx.fillRect(10, 10, 100, 100);
+	}
+}
+dino.draw();
