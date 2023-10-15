1cd777f55068c41abdebd7219a1b9bbfcfe23e9f
mamomental <healhunter@gmail.com>
2023-10-15 12:01:25 +0900

    Update cactus.js

--- 001/js/cactus.js
+++ 001/js/cactus.js
@@ -1,4 +1,4 @@
-export class Cactus {
+class CACTUS {
   constructor() {
   	this.x = 500;
   	this.y = 200;
@@ -9,4 +9,11 @@ export class Cactus {
     ctx.fillStyle = 'red';
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
+}
+
+export var CACTUSMANAGER = {
+	cactus : new CACTUS(),
+	draw(ctx) {
+		this.cactus.draw(ctx);
+	}
 }
