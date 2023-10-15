2694a7b3a6d378262c3dd469abf24b7e6b569f9e
mamomental <healhunter@gmail.com>
2023-10-15 11:25:34 +0900

    Update main.js

--- dino/js/main.js
+++ dino/js/main.js
@@ -6,5 +6,14 @@ var ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
-DINO.draw(ctx);
+
+function runFrame() {
+  requestAnimationFrame(runFrame);
+
+  DINO.x++;
+  DINO.draw(ctx);
 cactus.draw(ctx);
+
+}
+
+runFrame();
