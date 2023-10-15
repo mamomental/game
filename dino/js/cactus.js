1214d09f7a8f8e0ee8917c9d23bf936cbfc6da91
mamomental <healhunter@gmail.com>
2023-10-15 11:11:47 +0900

    Create cactus.js

---
+++ dino/js/cactus.js
@@ -0,0 +1,14 @@
+class Cactus = {
+  constructor() {
+  	x : 500,
+  	y : 200,
+  	width : 50,
+  	height : 50
+  }
+	draw(ctx) {
+		ctx.fillStyle = 'green';
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+export var cactus = new Cactus();
