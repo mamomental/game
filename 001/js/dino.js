d9ad9db026fc26a253846a1382b10ddf5ee5e335
mamomental <healhunter@gmail.com>
2023-10-15 10:23:14 +0900

    Create dino.js

---
+++ 001/js/dino.js
@@ -0,0 +1,12 @@
+var dino = {
+	x : 10,
+	y : 200,
+	width : 50,
+	height : 50,
+	draw() {
+		ctx.fillStyle = 'green';
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+export default dino;
