f76e12ddb676e30338e0f8eeaf1f7876e468a0b7
mamomental <healhunter@gmail.com>
2023-10-15 11:28:44 +0900

    Create cactus.js

---
+++ 001/js/cactus.js
@@ -0,0 +1,14 @@
+class Cactus {
+  constructor() {
+  	this.x = 500;
+  	this.y = 200;
+  	this.width = 50;
+  	this.height = 50;
+  }
+	draw(ctx) {
+		ctx.fillStyle = 'red';
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+export var CACTUS = new Cactus();
