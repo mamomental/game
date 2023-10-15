935b7d2d88eebd86c979ef3692158c630e8142d1
mamomental <healhunter@gmail.com>
2023-10-15 11:12:17 +0900

    Update main.js

--- dino/js/main.js
+++ dino/js/main.js
@@ -1,4 +1,5 @@
 import {DINO} from "./dino.js";
+import {cactus} from "./cactus.js";
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -6,3 +7,4 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 DINO.draw(ctx);
+cactus.draw(ctx);
