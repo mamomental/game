6a827816d81372eecedd2c2c00b9a39e8bebf3e8
mamomental <healhunter@gmail.com>
2023-10-15 11:04:17 +0900

    Create main.js

---
+++ dino/js/main.js
@@ -0,0 +1,8 @@
+import {DINO} from "./dino.js";
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+DINO.draw(ctx);
