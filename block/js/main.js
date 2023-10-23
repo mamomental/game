be42ef6dbf71f42576a7b66003db382fafe8ade8
mamomental <healhunter@gmail.com>
2023-10-23 14:25:12 +0900

    Create main.js

---
+++ block/js/main.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
