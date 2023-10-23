dffe3db13104114b828218aa0d5e6ca621a0fc74
mamomental <healhunter@gmail.com>
2023-10-23 14:29:07 +0900

    Update main.js

--- block/js/main.js
+++ block/js/main.js
@@ -6,3 +6,8 @@ ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
