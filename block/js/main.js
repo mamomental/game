71c168df850ef8263acf6ccaad57746809cc4e43
mamomental <healhunter@gmail.com>
2023-10-23 14:38:52 +0900

    Update main.js

--- block/js/main.js
+++ block/js/main.js
@@ -1,14 +1,15 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -17,6 +18,12 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
   x += dx;
   y += dy
 }
