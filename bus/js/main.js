3c9b473a6c5e4a2a748b17fbda4877a90e1f156d
mamomental <healhunter@gmail.com>
2023-11-08 11:20:45 +0900

    Update main.js

--- bus/js/main.js
+++ bus/js/main.js
@@ -10,17 +10,17 @@ jQuery(document).ready(function($) {
 			console.log(xml);
 			$(xml).find('busArrivalList').each(function(index){ 
                 	var rId = $(this).find('routeId').text();
-				if (rId == routeId) {
-					// 몇 번째 전
-					$('#locationNo1').text($(this).find('locationNo1').text());
-					$('#locationNo2').text($(this).find('locationNo2').text());
-					// 몇 분 후
-					$('#predictTime1').text($(this).find('predictTime1').text());
-					$('#predictTime2').text($(this).find('predictTime2').text());
-					// 빈 자리
-					$('#remainSeatCnt1').text($(this).find('remainSeatCnt1').text());
-					$('#remainSeatCnt2').text($(this).find('remainSeatCnt2').text());
-				}
+			if (rId == routeId) {
+				// 몇 번째 전
+				$('#locationNo1').text($(this).find('locationNo1').text());
+				$('#locationNo2').text($(this).find('locationNo2').text());
+				// 몇 분 후
+				$('#predictTime1').text($(this).find('predictTime1').text());
+				$('#predictTime2').text($(this).find('predictTime2').text());
+				// 빈 자리
+				$('#remainSeatCnt1').text($(this).find('remainSeatCnt1').text());
+				$('#remainSeatCnt2').text($(this).find('remainSeatCnt2').text());
+			}
 			});
 		}
 
@@ -35,7 +35,7 @@ jQuery(document).ready(function($) {
 						'routeId':routeId,
 						'staOrder':staOrder},
 					success : function (result) {
-						_parseData(result);
+						_parseData(result, routeId);
 					},
 					error: function(xhr,status,error){
 						console.log('code:'+xhr.status);
