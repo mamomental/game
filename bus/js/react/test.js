f6722f4745a5b3ef174e996ea04d27faca77ae33
mamomental <healhunter@gmail.com>
2023-11-13 13:59:09 +0900

    Create test.js

---
+++ bus/js/react/test.js
@@ -0,0 +1,11 @@
+ class App extends React.Component {
+	render() {
+		return (
+		<div>
+			<h1>Hello readers, Thankyou for reading this blog !</h1>
+		</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
