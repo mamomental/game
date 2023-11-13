5774eae9dc38ba0e896da7a89b67da36be432de4
mamomental <healhunter@gmail.com>
2023-11-14 08:06:43 +0900

    Update index.js

--- bus/react/js/index.js
+++ bus/react/js/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Gyeonggi from './gyeonggi.js';
 
 class App extends Component {
 	render() {
 		return (
 		<div>
 			<h1>Hello!</h1>
-			<Gyeonggi></Gyeonggi>
 		</div>
 		);
 	}
