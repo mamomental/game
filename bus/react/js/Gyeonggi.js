a26745ce1ebb054db7d9346dfdaff6b663f91674
mamomental <healhunter@gmail.com>
2023-11-14 09:06:38 +0900

    Update and rename gyeonggi.js to Gyeonggi.js

---
+++ bus/react/js/Gyeonggi.js
@@ -0,0 +1,3 @@
+export default function Gyeonggi() {
+  return <div>Gyeonggi</div>;
+}
--- bus/react/js/gyeonggi.js
+++
@@ -1,9 +0,0 @@
-class Gyeonggi extends React.Component {
-    render() {
-        return (
-          <div>Gyeonggi</div>
-        );
-    }
-}
-
-export default Gyeonggi;
