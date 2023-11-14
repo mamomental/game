24440ea9adc8e1568856efc1c25dfc0f078fdc15
mamomental <healhunter@gmail.com>
2023-11-14 09:06:02 +0900

    Create App.js

---
+++ bus/react/js/App.js
@@ -0,0 +1,11 @@
+import Test from "./Gyeonggi.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Gyeonggi></Gyeonggi>
+    </div>
+  );
+}
