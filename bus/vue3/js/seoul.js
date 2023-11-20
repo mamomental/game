3ac5e7419ab263779ef7b38adcbc86b9e7e1419f
mamomental <healhunter@gmail.com>
2023-11-20 14:22:22 +0900

    Update seoul.js

--- bus/vue3/js/seoul.js
+++ bus/vue3/js/seoul.js
@@ -78,7 +78,7 @@ const seoul = {
 	template: `<span>
 	<span>{{this.timer.counter}}</span>
 	<table id="bustable">
-		<caption>서울 버스 도착 정보(샘플)</caption>
+		<caption>서울 버스 도착 정보</caption>
 		<thead>
 			<tr>
 				<th scope="col">정거장</th>
