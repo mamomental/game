31f1edfd50193fe3f3122b1faa45dbfcbc1fd1a6
mamomental <healhunter@gmail.com>
2024-01-18 05:26:17 +0900

    Update seoul.js

--- bus/vue3/js/seoul.js
+++ bus/vue3/js/seoul.js
@@ -77,8 +77,8 @@ const seoul = {
 		<tbody>
 			<tr v-for="row in tableRow">
 				<th scope="row">{{row.busNo}}번</th>
-				<td scope="row">{{row.predictTime1}}</th>
-				<td scope="row">{{row.predictTime2}}</th>
+				<td scope="row">{{row.predictTime1}}</td>
+				<td scope="row">{{row.predictTime2}}</td>
 			</tr>
 		</tbody>
 	</table>
